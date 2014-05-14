9d2146edf1384a33d5dbd1e8d236f3aca49ca8b1
Ilker Mengi <ilk.er_55@live.nl>
2014-05-14 10:49:51 +0200

    Updates

--- Swopp2/gen/nl/creationinc/swopp2/R.java
+++ Swopp2/gen/nl/creationinc/swopp2/R.java
@@ -1068,9 +1068,11 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int ic_drawer=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
+        public static final int accessoires=0x7f05003f;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1090,6 +1092,7 @@ containing a value of this type.
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int drawer_layout=0x7f05003c;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1100,6 +1103,8 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int kleding=0x7f05003d;
+        public static final int left_drawer=0x7f050040;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1123,6 +1128,7 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int sieraden=0x7f05003e;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1183,7 +1189,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int drawer_listview_item=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1226,8 +1233,13 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int accessoires=0x7f0a0013;
         public static final int app_name=0x7f0a000d;
+        public static final int drawer_close=0x7f0a0010;
+        public static final int drawer_open=0x7f0a000f;
         public static final int hello_world=0x7f0a000e;
+        public static final int kleding=0x7f0a0011;
+        public static final int sieraden=0x7f0a0012;
     }
     public static final class style {
         /** 
--- Swopp2/src/nl/creationinc/swopp2/MainActivity.java
+++ Swopp2/src/nl/creationinc/swopp2/MainActivity.java
@@ -1,15 +1,145 @@
 package nl.creationinc.swopp2;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private String[] mDrawerListViewItems;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
+    private ActionBarDrawerToggle mDrawerToggle;
+    
+    //AppPreferences _appPrefs;
+    int user_id;
+	private String username;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		String[] items = {
+	        "User",
+	        "About"
+	    };
+	
+		mDrawerListViewItems = items;
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+	    mDrawerListView = (ListView) findViewById(R.id.left_drawer);
+	    mDrawerListView.setAdapter(new ArrayAdapter<String>(this,
+	            R.layout.drawer_listview_item, mDrawerListViewItems));
+	    mDrawerToggle = new ActionBarDrawerToggle(
+	            this,                  /* host Activity */
+	            mDrawerLayout,         /* DrawerLayout object */
+	            R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+	            R.string.drawer_open,  /* "open drawer" description */
+	            R.string.drawer_close  /* "close drawer" description */
+	            ){
+    	
+    	/** Called when a drawer has settled in a completely closed state. */
+        public void onDrawerClosed(View view) {
+            super.onDrawerClosed(view);
+            getActionBar().setTitle(R.string.drawer_close);
+        }
+
+        /** Called when a drawer has settled in a completely open state. */
+        public void onDrawerOpened(View drawerView) {
+            super.onDrawerOpened(drawerView);
+            getActionBar().setTitle(R.string.drawer_open);
+        }
+        
+	    };
+        
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        //drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        mDrawerListView.setOnItemClickListener(new DrawerItemClickListener());
+        
+        findViewById(R.id.kleding).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//startActivity(new Intent (MainActivity.this, Kleding.class));
+			}
+		});
+  		findViewById(R.id.sieraden).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//startActivity(new Intent (MainActivity.this, Sieraden.class));
+			}
+		});
+  		findViewById(R.id.accessoires).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//startActivity(new Intent (MainActivity.this, Accessoires.class));
+			}
+		});
 	}
+	
+	/** Start ActionBarDrawer */
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+	
+	@Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+	
+	/*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        
+        switch (item.getItemId()) {
+		    case R.id.action_settings:
+		    	//Intent settings = new Intent(this, SettingsActivity.class);
+				//startActivity(settings);
+		        return true;
+		    case R.id.action_log_out:
+		    	signOut();
+		    	return true;
+		    default:
+		        return super.onOptionsItemSelected(item);
+        }
+    }*/
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @SuppressWarnings("rawtypes")
+		@Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+        	switch (position) {
+		    	case 0:
+		    		/*Intent profile = new Intent(MainActivity.this, ProfileActivity.class);
+		    		profile.putExtra("userid", user_id);
+					startActivity(profile);*/
+		    		Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_LONG).show();
+					break;
+		    	case 1:
+		    		/*Intent about = new Intent(MainActivity.this, AboutActivity.class);
+					startActivity(about);*/
+		    		Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_LONG).show();
+		    		break;
+        	}
+        }
+    }
+	/** End ActionBarDrawer */
 }
