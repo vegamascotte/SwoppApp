840d4b75ed8930f2584f8107e481ed59098d2688
Arzana <largewowfan@gmail.com>
2014-06-04 14:51:26 +0200

    Update
    Fetch werkend

--- Swopp2/src/nl/creationinc/swopp2/Clothing.java
+++ Swopp2/src/nl/creationinc/swopp2/Clothing.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import com.parse.FindCallback;
@@ -22,98 +23,66 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Clothing extends Activity
 {
 	int stackIndex = 0;
-	int index = -1;
-	swopObject[] swopObjects = new swopObject[10];
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_clothing);
-		swopObjects[9] = new swopObject();
 
-		ParseQuery<ParseObject> query = ParseQuery.getQuery("Products");
-		try
-		{
-			while (true)
-				FillArray(query);
-		} catch (Exception e)
-		{
-			Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG)
-					.show();
-		}
-	}
-
-	protected void FillArray(ParseQuery<ParseObject> query)
-	{
-		try
-		{
-			if (index != -1)
-				SetImage(swopObjects[0].getImgUrl(), "Products");
-
-			if (HasEmptySpot())
-			{
-				query.whereEqualTo("stack", stackIndex);
-				query.findInBackground(new FindCallback<ParseObject>()
-				{
-					@Override
-					public void done(List<ParseObject> objects, ParseException e)
+		try {
+			findViewById(R.id.next_button).setOnClickListener(
+					new View.OnClickListener()
 					{
-						stackIndex++;
-						index = 0;
-
-						for (int i = 0; i < swopObjects.length || i < objects.size(); i++)
-						{
-							try
-							{
-								swopObjects[i] = new swopObject(objects.get(i));
-							} catch (Exception ex)
-							{
-								Toast.makeText(getBaseContext(), ex.toString(),
+						@Override
+						public void onClick(View v) {
+							try {
+								swopObject obj = UpdateStack();
+								SetSwopObject(obj.getImgUrl(), obj.getClass()
+										.getName());
+								((TextView) findViewById(R.id.textView1))
+										.setText(obj.getName());
+								stackIndex++;
+							}
+							catch (Exception e) {
+								Toast.makeText(getBaseContext(), e.toString(),
 										Toast.LENGTH_LONG).show();
 							}
 						}
-					}
-				});
-			}
-		} catch (Exception e)
-		{
+					});
+		}
+		catch (Exception e) {
 			Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG)
 					.show();
 		}
+
+		findViewById(R.id.next_button).performClick();
 	}
 
-	protected boolean HasEmptySpot()
-	{
-		for (swopObject cur : swopObjects)
-		{
-			if (cur == null)
-				return true;
-		}
-		return false;
+	protected swopObject UpdateStack() throws ParseException {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Products").whereEqualTo("stack", stackIndex);
+		return new swopObject(query.getFirst());
 	}
 
-	protected void SetImage(String url, String className)
-	{
-		String[] params =
-		{ url, className };
-		try
-		{
+	protected void SetSwopObject(String url, String className) {
+		String[] params = { url, className };
+		try {
 			Drawable image = ((Utility) new Utility().execute(params)).get();
 			((ImageView) findViewById(R.id.mainImgView))
 					.setImageDrawable(image);
-		} catch (InterruptedException e)
-		{
+		}
+		catch (InterruptedException e) {
 			Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG)
 					.show();
-		} catch (ExecutionException e)
-		{
+		}
+		catch (ExecutionException e) {
 			Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG)
 					.show();
 		}
